90374e40bd9da92aa00e1a702c6dca9492d87342
Jéssica Santos <111154078+jessiysantos@users.noreply.github.com>
2025-05-09 22:48:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
+You said:
+import streamlit as st
+import sys
+import os
+
+# Adicionando o diretório 'pages' ao caminho de busca de módulos
+current_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(current_dir, 'pages'))
+
+# Configuração da página
+st.set_page_config(page_title="Página inicial", page_icon=":guardsman:", layout="wide")
+
+# Título
+st.title("🎯 Sistema de Recomendação de Candidatos")
+st.markdown("Seja bem-vindo(a) selecione a opção desejada.")
 import streamlit as st
 import requests
 import io
