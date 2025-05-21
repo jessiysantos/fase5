8fae6dd49bffc070e095a50b6a80c2dcf3fd41eb
Fernanda Castilho <57471859+fernandancastilho@users.noreply.github.com>
2025-05-21 02:25:42 -0300

    Update Decision_Match_Hub.py

--- pages/Decision_Match_Hub.py
+++ pages/Decision_Match_Hub.py
@@ -372,7 +372,8 @@ part1 = pd.read_parquet("applicants_part1.parquet")
 #part3 = pd.read_parquet("applicants_part3.parquet")
 
 # Junta tudo em um único DataFrame
-df = pd.concat([part1, part2, part3], ignore_index=True)
+#df = pd.concat([part1, part2, part3], ignore_index=True)
+df = pd.concat([part1, part2], ignore_index=True)
 prospects = carregar_prospects()
 jobs_data = load_jobs_data()
 
